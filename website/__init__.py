c330bc915623b31b0a3e5054fc50735fe5d660d3
antonistsou <64914509+antonistsou@users.noreply.github.com>
2022-11-21 12:38:54 +0200

    Select button with javascript and small html changes

--- website/__init__.py
+++ website/__init__.py
@@ -8,29 +8,35 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__) 
+    #database key 
     app.config['SECRET_KEY'] = 'fsefgsergsgrgb'
+    #database location 
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
     db.init_app(app)
 
+
     from .views import views
     from .auth import auth
 
+    #blueprints prefix url 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User , Events
+    from .models import User 
 
     #db creation
     with app.app_context():
         if not path.exists('instance/' + DB_NAME):
             db.create_all()
             print('_________________ DB created! ___________________')
-    
+
+    #redirect if login required : login page
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
+    #user load that relates to this id 
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint , render_template
+from flask import Blueprint , render_template ,request , flash ,redirect, url_for
 from flask_login import  login_required , current_user
 
 
@@ -8,3 +8,8 @@ views = Blueprint('views' , __name__)
 @login_required
 def home():
     return render_template("home.html" , user = current_user)
+
+
+@views.route('/result' , methods = ['GET' , 'POST'])
+def result():
+    return render_template("result.html" , user = current_user)
