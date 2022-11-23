4a659bf915a9466eb503d307b9572a65b2138c2d
antonistsou <64914509+antonistsou@users.noreply.github.com>
2022-11-23 17:08:15 +0200

    Thessaloniki guide web scrapping almost complete

--- website/models.py
+++ website/models.py
@@ -1,5 +1,7 @@
 from . import db
 from flask_login import UserMixin
+from bs4 import BeautifulSoup
+
 
 # User db scheme
 class User(db.Model , UserMixin):
@@ -7,3 +9,16 @@ class User(db.Model , UserMixin):
     email = db.Column(db.String(150), unique = True)
     firstName = db.Column(db.String(150))
     password = db.Column(db.String(150))
+
+class Event:
+    def __init__(self, id, name, image, description, location , day , time ):
+        self.id=id
+        self.name = name
+        self.image = image
+        self.description = description
+        self.location = location
+        self.day = day 
+        self.time = time
+
+    
+
